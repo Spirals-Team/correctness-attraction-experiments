b25cc8dcdd4f874793d7c71c04bf7277306a7911
bdanglot <bdanglot@gmail.com>
2016-04-16 21:00:19 +0200

    fix calling uncompress.sh

--- src/script/install.sh
+++ src/script/install.sh
@@ -14,4 +14,4 @@ rm -Rf commons-math3-3.6.1-src
 java -classpath spoon-core-5.1.0-jar-with-dependencies.jar:jPerturb/target/jPerturb-0.0.1-SNAPSHOT.jar spoon.Launcher -i resources/java:jPerturb/src/main/java/perturbation/ -o src/main/java -x --with-imports -p processor.AssignmentProcessor:processor.VariableCaster:processor.PerturbationProcessor
 mvn compile
 gcc ./src/script/emps.c -o emps
-./src/script/uncompressh.sh
+./src/script/uncompress.sh
