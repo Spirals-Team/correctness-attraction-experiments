a39db5697705646ee18f4609f896eb657199c198
danglotb <bdanglot@gmail.com>
2016-04-18 10:36:24 +0200

    reduce the number of task in optimizer

--- src/script/run.sh
+++ src/script/run.sh
@@ -4,4 +4,4 @@ java -cp target/classes quicksort.Main
 java -cp target/classes zip.Main
 java -cp target/classes md5.Main
 java -cp target/classes sudoku.Main
-java -cp target/classes optimizer.Main
+java -cp target/classes optimizer.Main -nb 1
