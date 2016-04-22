87ce3d5f3e1b22f7b4f188c6abd11aaf007265b1
danglotb <bdanglot@gmail.com>
2016-04-22 09:00:23 +0200

    set up

--- src/script/commit.sh
+++ src/script/commit.sh
@@ -3,6 +3,8 @@
 git config --global user.email "bdanglot@gmail.com"
 git config --global user.name "bdanglot"
 
+git checkout -b auto-push
+
 git add results/
 git commit -m "Commit new results"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:test_auto_push > /dev/null 2>&1
+git push origin auto-push
