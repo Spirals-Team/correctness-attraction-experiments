20ccfe932e4167affcba6664677e63c5ba49a118
danglotb <bdanglot@gmail.com>
2016-04-22 15:55:59 +0200

    set Utils exp

--- src/main/java/torrent/Main.java
+++ src/main/java/torrent/Main.java
@@ -1,8 +1,7 @@
 package torrent;
 
 import com.turn.ttorrent.bcodec.BDecoder;
-import com.turn.ttorrent.client.Client;
-import experiment.AddOneExplorerImpl;
+import com.turn.ttorrent.common.Utils;
 import experiment.Runner;
 import experiment.Util;
 
@@ -13,7 +12,7 @@ public class Main {
 
     public static void run() {
         System.out.println("Run Torrent...");
-        Runner.setup(BDecoder.class, TorrentCallable.class, new TorrentManager(), 0.3f, String.class);
+        Runner.setup(Utils.class, TorrentCallable.class, new TorrentManager(), String.class);
         Runner.runExplorers();
         ((TorrentManager)Runner.manager).stop();
         System.exit(0);//need to force exit because sometimes the torrent doesn't stop itself
