3a072ef33c1cf6aeeac69d0917f347c05f754a42
danglotb <bdanglot@gmail.com>
2016-04-22 13:52:06 +0200

    add Current branch

--- src/script/commit.sh
+++ src/script/commit.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+CURRENT_BRANCH=($(git rev-parse --abbrev-ref HEAD))
+
 git config --global user.email "bdanglot@gmail.com"
 git config --global user.name "bdanglot"
 
-#git checkout -b auto-push
+git checkout -b auto-push
 
 git add results/
 git commit -m "Commit new results"
-git push "https://${GH_TOKEN}@${GH_REF}" branch-results
+git push "https://${GH_TOKEN}@${GH_REF}" $CURRENT_BRANCH:branch-results
