9c3c3d36711d242117c7eeea24d00e023faa5ff0
danglotb <bdanglot@gmail.com>
2016-04-22 16:04:56 +0200

    add git pull before push

--- src/script/commit.sh
+++ src/script/commit.sh
@@ -5,8 +5,9 @@ CURRENT_BRANCH=($(git rev-parse --abbrev-ref HEAD))
 git config --global user.email "bdanglot@gmail.com"
 git config --global user.name "bdanglot"
 
-git checkout -b auto-push
-
 git add results/
 git commit -m "Commit new results"
+
+git pull branch-results
+
 git push "https://${GH_TOKEN}@${GH_REF}" $CURRENT_BRANCH:branch-results
