6325dd3b79c30935da9d11e237a5adc262262c02
danglotb <bdanglot@gmail.com>
2016-04-22 09:48:13 +0200

    change mvn dependencies

--- src/script/installTorrent.sh
+++ src/script/installTorrent.sh
@@ -8,12 +8,9 @@ processors=processor.AssignmentProcessor:processor.VariableCaster:processor.Pert
 git clone https://github.com/mpetazzoni/ttorrent.git
 cd ttorrent
 mvn package -Dmaven.javadoc.skip=true
+lib=($(mvn dependency:build-classpath | grep -v '\[INFO\]'))
 cd ..
 mv ttorrent/core/target/ttorrent-core-1.6-SNAPSHOT.jar ./
-
-#M2_REPO=($(mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]' | tail -n 1))
-M2_REPO=$HOME/.m2
-lib=$M2_REPO/commons-io/commons-io/2.4/commons-io-2.4.jar:$M2_REPO/org/simpleframework/simple/4.1.21/simple-4.1.21.jar:$M2_REPO/org/slf4j/slf4j-api/1.6.4/slf4j-api-1.6.4.jar:$M2_REPO/org/slf4j/slf4j-log4j12/1.6.4/slf4j-log4j12-1.6.4.jar
 
 path=ttorrent/core/src/main/java/
 files=(com/turn/ttorrent/client/Client.java)
