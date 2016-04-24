174172490c7a303fa04173eccf1605040af8e2b5
bdanglot <bdanglot@gmail.com>
2016-04-24 19:32:14 +0200

    change exp

--- src/main/java/torrent/Main.java
+++ src/main/java/torrent/Main.java
@@ -1,6 +1,6 @@
 package torrent;
 
-import com.turn.ttorrent.client.storage.FileStorage;
+import com.turn.ttorrent.*;
 import experiment.Runner;
 import experiment.Util;
 
@@ -11,7 +11,7 @@ public class Main {
 
     public static void run() {
         System.out.println("Run Torrent...");
-        Runner.setup(FileStorage.class, TorrentCallable.class, new TorrentManager(), 0.1f, String.class);
+        Runner.setup(Client.class, TorrentCallable.class, new TorrentManager(), 0.1f, String.class);
         Runner.runExplorers();
         ((TorrentManager)Runner.manager).stop();
         System.exit(0);//need to force exit because sometimes the torrent doesn't stop itself
