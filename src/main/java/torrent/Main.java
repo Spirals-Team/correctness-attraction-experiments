711360b2d35ee6a39352d52ee8533e433da02999
bdanglot <bdanglot@gmail.com>
2016-04-24 18:15:15 +0200

    set up exp

--- src/main/java/torrent/Main.java
+++ src/main/java/torrent/Main.java
@@ -1,5 +1,6 @@
 package torrent;
 
+import com.turn.ttorrent.bcodec.BDecoder;
 import com.turn.ttorrent.common.Utils;
 import experiment.Runner;
 import experiment.Util;
@@ -11,7 +12,7 @@ public class Main {
 
     public static void run() {
         System.out.println("Run Torrent...");
-        Runner.setup(Utils.class, TorrentCallable.class, new TorrentManager(), String.class);
+        Runner.setup(BDecoder.class, TorrentCallable.class, new TorrentManager(), 0.1f, String.class);
         Runner.runExplorers();
         ((TorrentManager)Runner.manager).stop();
         System.exit(0);//need to force exit because sometimes the torrent doesn't stop itself
--- src/script/commit.sh
+++ src/script/commit.sh
@@ -5,12 +5,6 @@ CURRENT_BRANCH=($(git rev-parse --abbrev-ref HEAD))
 git config --global user.email "bdanglot@gmail.com"
 git config --global user.name "bdanglot"
 
-echo "branches:" >> .travis.yml
-echo "except:" >> .travis.yml
-echo "-" $CURRENT_BRANCH >> .travis.yml
-
-git add .travis.yml
-
 git add results/
 git commit -m "Commit new results"
-git push "https://${GH_TOKEN}@${GH_REF}" $CURRENT_BRANCH:$CURRENT_BRANCH
+git push "https://${GH_TOKEN}@${GH_REF}" $CURRENT_BRANCH:branch-results-torrent
