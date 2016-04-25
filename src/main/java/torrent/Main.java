7adb595ef45984690b3f12e84ea30d259f830d0e
danglotb <bdanglot@gmail.com>
2016-04-25 14:55:45 +0200

    next exp

--- src/main/java/torrent/Main.java
+++ src/main/java/torrent/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void run() {
         System.out.println("Run Torrent...");
-        Runner.setup(BDecoder.class, TorrentCallable.class, new TorrentManager(), String.class);
+        Runner.setup(BDecoder.class, TorrentCallable.class, new TorrentManager(), 0.1f, 1, String.class);
         Runner.runExplorers();
         ((TorrentManager)Runner.manager).stop();
         System.exit(0);//need to force exit because sometimes the torrent doesn't stop itself
