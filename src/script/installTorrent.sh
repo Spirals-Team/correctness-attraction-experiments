92d99e80751f62d4a954910ae19d7027f34afd0a
danglotb <bdanglot@gmail.com>
2016-04-25 09:21:14 +0200

    fix install torrent

--- src/script/installTorrent.sh
+++ src/script/installTorrent.sh
@@ -9,17 +9,21 @@ git clone https://github.com/mpetazzoni/ttorrent.git
 cd ttorrent
 mvn package -Dmaven.javadoc.skip=true
 lib=($(mvn dependency:build-classpath | grep -v '\[INFO\]'))
+echo $lib
 cd ..
-mv ttorrent/core/target/ttorrent-core-1.6-SNAPSHOT.jar ./
 
+ttorrentjar=ttorrent/core/target/ttorrent-core-1.6-SNAPSHOT.jar
 path=ttorrent/core/src/main/java/
 files=(com/turn/ttorrent/)
+output=src/main/java/
+
+echo $lib:$ttorrentjar:$spoon:$jPerturb
 
 for i in "${files[@]}"
 do
-   echo $i
-   java -classpath $lib:ttorrent-core-1.6-SNAPSHOT.jar:$spoon:$jPerturb spoon.Launcher -i $path/$i:$perturbation -o $path -p $processors
+   echo spooning $i
+   java -classpath $lib:$ttorrentjar:$spoon:$jPerturb spoon.Launcher -i $path/$i:$perturbation -o $output -p $processors
 done
 
-cp -R ttorrent/core/src/main/java/com/ src/main/java/
+#cp -R ttorrent/core/src/main/java/com/ src/main/java/
 #rm -Rf ttorrent
