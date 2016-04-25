7478e10c34baa1b8aba380fcae330a73626bd22c
bdanglot <bdanglot@gmail.com>
2016-04-25 18:52:40 +0200

    reduce the exp

--- src/main/java/torrent/Main.java
+++ src/main/java/torrent/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void run() {
         System.out.println("Run Torrent...");
-        Runner.setup(BDecoder.class, TorrentCallable.class, new TorrentManager(), String.class);
+        Runner.setup(BDecoder.class, TorrentCallable.class, 0.5f, 0, new TorrentManager(), String.class);
         Runner.runExplorers();
         ((TorrentManager) Runner.manager).stop();
         System.exit(0);//need to force exit because sometimes the torrent doesn't stop itself
