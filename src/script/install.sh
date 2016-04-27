e1140ac9aa84a9421a36b35a55d80b9bddb03f24
bdanglot <bdanglot@gmail.com>
2016-04-27 17:24:54 +0200

    using jPerturb with dependencies

--- src/script/install.sh
+++ src/script/install.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-spoon=spoon-core-5.1.0-jar-with-dependencies.jar
-jPerturb=jPerturb/target/jPerturb-0.0.1-SNAPSHOT.jar
+jPerturb=jPerturb/target/jPerturb-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 perturbation=jPerturb/src/main/java/perturbation/
 processors=processor.AssignmentProcessor:processor.VariableCaster:processor.RenameProcessor:processor.PerturbationProcessor
 
 path=src/main/java/
 
-git clone http://github.com/Spirals-Team/jPerturb.git
-cd jPerturb
-mvn install
+git clone https://github.com/INRIA/spoon.git
+cd spoon
+mvn install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
+
 cd ..
 
-wget https://gforge.inria.fr/frs/download.php/latestzip/86/Spoon-latest.zip
-unzip Spoon-latest.zip 2>/dev/null 1>/dev/null
+git clone http://github.com/Spirals-Team/jPerturb.git
+cd jPerturb
+mvn package -Dmaven.test.skip=true
+
+cd ..
 
 wget http://mirror.trisect.eu/Apache//commons/math/source/commons-math3-3.6.1-src.zip
 unzip commons-math3-3.6.1-src.zip 2>/dev/null 1>/dev/null
@@ -26,7 +29,5 @@ files=(org/apache/commons/math3/optim/linear/SimplexSolver.java)
 for i in "${files[@]}"
 do
     echo $i
-    java -classpath $spoon:$jPerturb spoon.Launcher -i $path/$i:$perturbation -o src/main/java -x --with-imports -p $processors
+    java -jar $jPerturb -i $path/$i:$perturbation -o src/main/java -x --with-imports -p $processors
 done
-
-mvn compile
