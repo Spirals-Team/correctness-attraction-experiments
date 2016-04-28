555d28288fad4cf33a3f9d4e74736f491501f75d
bdanglot <bdanglot@gmail.com>
2016-04-28 15:10:36 +0200

    commons math is used as dependencies

--- src/script/install.sh
+++ src/script/install.sh
@@ -2,32 +2,41 @@
 
 jPerturb=jPerturb/target/jPerturb-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 perturbation=jPerturb/src/main/java/perturbation/
-processors=processor.AssignmentProcessor:processor.VariableCaster:processor.RenameProcessor:processor.PerturbationProcessor
+processors=processor.AssignmentProcessor:processor.VariableCaster:processor.PerturbationProcessor
 
-path=src/main/java/
-
+#Install Sponn
 git clone https://github.com/INRIA/spoon.git
 cd spoon
 mvn install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
 
 cd ..
 
+#Install jPerturb
 git clone http://github.com/Spirals-Team/jPerturb.git
 cd jPerturb
 mvn package -Dmaven.test.skip=true
 
 cd ..
 
-wget http://mirror.trisect.eu/Apache//commons/math/source/commons-math3-3.6.1-src.zip
+#Install Commons Math
+wget http://mirror.trisect.eu/Apache/commons/math/source/commons-math3-3.6.1-src.zip
 unzip commons-math3-3.6.1-src.zip 2>/dev/null 1>/dev/null
+cd commons-math3-3.6.1-src
+mvn package -DskipTests -Dmaven.javadoc.skip=true
+jar_math=commons-math3-3.6.1-src/target/commons-math3-3.6.1.jar
+path_math=commons-math3-3.6.1-src/src/main/java/
 
-cp -R commons-math3-3.6.1-src/src/main/java/org/ $path
-rm -Rf commons-math3-3.6.1-src
+cd ..
 
-#files=(md5/MD5.java quicksort/QuickSort.java mersenne/MersenneTwister.java org/apache/commons/math3/optim/linear/SimplexSolver.java sudoku/Sudoku.java zip/LZW.java)
-files=(org/apache/commons/math3/optim/linear/SimplexSolver.java)
-for i in "${files[@]}"
-do
-    echo $i
-    java -jar $jPerturb -i $path/$i:$perturbation -o src/main/java -x --with-imports -p $processors
-done
+#Spooning math
+i=org/apache/commons/math3/optim/linear/SimplexSolver.java
+echo "java -cp $jar_math:$jPerturb spoon.Launcher -i $path_math/$i:$perturbation -o $path_math --with-imports -p processor.RenameProcessor:$processors"
+java -cp $jar_math:$jPerturb spoon.Launcher -i $path_math/$i:$perturbation -o $path_math --with-imports -p processor.RenameProcessor:$processors
+
+#Install Instrumented Math
+cd commons-math3-3.6.1-src
+mvn install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -q
+
+cd ..
+
+mvn compile
