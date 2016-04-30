47ce6ef210da56ac50e19f5664b8b1185191e20a
bdanglot <bdanglot@gmail.com>
2016-04-30 17:09:21 +0200

    fix rsa launcher

--- src/main/java/rsa/Main.java
+++ src/main/java/rsa/Main.java
@@ -1,17 +1,29 @@
 package rsa;
 
+import experiment.IntegerAdd1RndEnactorExplorerImpl;
 import experiment.Runner;
 import experiment.Util;
+import perturbation.perturbator.AddNPerturbatorImpl;
 
 /**
  * Created by bdanglot on 29/04/16.
  */
 public class Main {
 
+    private static final String QUALIFIED_NAME_RSACOREENGINE = "org.bouncycastle.crypto.engines.RSACoreEngine";
+
     public static void run() {
         System.out.println("Run RSA...");
-//        Runner.setup(RSACoreEngine.class,RSACallable.class,new RSAManager(), "Numerical", String.class);
-        Runner.runExplorers();
+        //RSACoreEngine is a friendly class
+        try {
+            Class rsaCoreEngine = ClassLoader.getSystemClassLoader().loadClass(QUALIFIED_NAME_RSACOREENGINE);
+            Runner.setup(rsaCoreEngine ,RSACallable.class,new RSAManager(), "Numerical", String.class);
+            Runner.verbose = true;
+            Runner.run(new IntegerAdd1RndEnactorExplorerImpl(new AddNPerturbatorImpl(1)));
+            //Runner.runExplorers();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
