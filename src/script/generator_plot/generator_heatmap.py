76e7efeb1507a963529589c93a0eb14b7da9fb3e
bdanglot <bdanglot@gmail.com>
2016-05-04 17:29:23 +0200

    new type of heatmap

--- src/script/generator_plot/generator_heatmap.py
+++ src/script/generator_plot/generator_heatmap.py
@@ -1,23 +1,45 @@
 import numpy as np
-import numpy.random
 import matplotlib.pyplot as plt
 
-x = []
-y = []
-success = []
+subject = "quicksort"
 
-nbPerloc = 1000
+lines = [line.rstrip('\n') for line in
+         open("results/" + subject + "/IntegerAddM_RandomExplorer_analysis_graph_data.txt")]
+magnitudes = ' '.join(lines[2].split()).split(" ")[3:]
+randoms = ' '.join(lines[5].split()).split(" ")[3:]
+numberOfLocation = ' '.join(lines[3].split()).split(" ")[0]
 
-lines = [line.rstrip('\n') for line in open("results/quicksort/IntegerAddM_HeatMap.txt")]
+x = [float(rnd) for rnd in randoms]
+y = [int(mag) for mag in magnitudes]
 
-for i in range(11):
-    for line in lines[8+(i*nbPerloc):8+((i+1)*nbPerloc)]:
-        l = ' '.join(line.split()).split(" ")
-        x.append(float(l[0].replace(',','.')))
-        y.append(float(l[1].replace(',','.')))
-        perc = float(l[-1].replace(',','.'))
-        print(perc / 100.0)
-        success.append((0.25 + perc/100.0, 0.0, 1 - (perc / 100.0)))
+i = 9
+nbLocation = 0
+mx, my = np.meshgrid(x, y)
 
-plt.scatter(x,y,color=success,alpha=0.3)
-plt.show()
+for loc in range(int(numberOfLocation)):
+    currentLoca = int(' '.join(lines[i].split()).split(" ")[2])
+    intensity = []
+    for zy in range(len(y)):
+        currentIntensity = []
+        for zx in range(len(x)):
+            currentIntensity.append(int(float(' '.join(lines[i + zx].split()).split(" ")[-1].replace(',', '.'))))
+        intensity.append(currentIntensity)
+        i = i + len(y)
+
+    print(x)
+    print(y)
+    print(intensity)
+
+    intensity = np.array(intensity)
+
+    fig = plt.figure()
+    ax = fig.add_axes((.1,.4,.8,.5))
+    plt.pcolormesh(mx, my, intensity)
+    plt.colorbar()
+    fig.savefig("results/" + subject + "/img/heatmap_"+str(currentLoca)+".pdf")
+    fig.savefig("results/" + subject + "/img/heatmap_"+str(currentLoca)+".jpeg")
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    fig.savefig("results/" + subject + "/img/heatmap_"+str(currentLoca)+"_log.pdf")
+    fig.savefig("results/" + subject + "/img/heatmap_"+str(currentLoca)+"_log.jpeg")
+
