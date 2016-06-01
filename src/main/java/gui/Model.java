357c331e5be0d3abb962ea1325122d236456178f
bdanglot <bdanglot@gmail.com>
2016-06-01 17:29:40 +0200

    add gui

--- src/main/java/gui/Model.java
+++ src/main/java/gui/Model.java
@@ -1,17 +1,23 @@
 package gui;
 
+import experiment.Manager;
+import perturbation.PerturbationEngine;
 import perturbation.enactor.NeverEnactorImpl;
 import perturbation.enactor.RandomEnactorImpl;
 import perturbation.location.PerturbationLocation;
-import perturbation.location.PerturbationLocationImpl;
+import perturbation.log.LoggerImpl;
 import perturbation.perturbator.AddNPerturbatorImpl;
-import quicksort.QuickSortInstr;
-import quicksort.QuickSortManager;
-import quicksort.QuickSortOracle;
+import perturbation.perturbator.InvPerturbatorImpl;
+import perturbation.perturbator.NothingPerturbatorImpl;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -19,74 +25,241 @@ import java.util.stream.IntStream;
  */
 public class Model {
 
-    private float rnd;
+    private float rnd = 0.0f;
 
-    private QuickSortManager manager;
-
-    private QuickSortOracle oracle;
+    private Manager manager;
 
     private List<PerturbationLocation> locations;
 
     private int numberOfTask;
+
     private int size;
 
-    public Model() {
+    private float step = 0.001f;
+
+    private Class<?> classManager;
+
+    private int accExec;
+
+    private int accExecSuccess;
+
+    private double avgPerturbationPerExec;
+
+    private List<String> classOfLocation;
+
+    private String currentTypeOfLocation;
+
+    private List<Integer> antifragileLocation;// == 100%
+
+    private List<Integer> robustLocation;//100% to 50%
+
+    private List<Integer> weakLocation;//<50%
+
+    public int getAccExec() {
+        return accExec;
+    }
+
+    public int getAccExecSuccess() {
+        return accExecSuccess;
+    }
+
+    public double getAvgPerturbationPerExec() {
+        return avgPerturbationPerExec;
+    }
+
+    public int getNumberOfTask() {
+        return numberOfTask;
+    }
+
+    public void setNumberOfTask(int numberOfTask) {
+        this.numberOfTask = numberOfTask;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setType(String type) {
+        this.currentTypeOfLocation = type;
+    }
+
+    public Model(Class<?> manager) {
+        this.accExec = 0;
+        this.accExecSuccess = 0;
         this.size = 100;
         this.numberOfTask = 40;
-        this.manager = new QuickSortManager(numberOfTask, size);
-        oracle = new QuickSortOracle();
-        locations = PerturbationLocationImpl.getLocationFromClass(QuickSortInstr.class)
-                .stream()
-                .filter(location -> location.getType().equals("Numerical"))
-                .collect(Collectors.toList());
-        locations.forEach(location -> location.setPerturbator(new AddNPerturbatorImpl(1)));
+        this.avgPerturbationPerExec = 0.0d;
+        this.classManager = manager;
+        this.classOfLocation = new ArrayList<>();
+        this.classOfLocation.add("Antifragile");
+        this.currentTypeOfLocation = "Numerical";
+        try {
+            this.manager = (Manager) this.classManager.getConstructor(int.class, int.class, int.class).newInstance(this.numberOfTask, this.size, (int) System.currentTimeMillis());
+            this.initLocations();
+            this.setUpLocations();
+            PerturbationEngine.loggers.put("gui", new LoggerImpl());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
-    public void setRnd(float rnd) {
-        this.rnd = rnd;
+    private void initLocations() {
+        this.locations = this.manager.getLocations();
+        this.locations.stream().filter(location -> !location.getType().equals("Boolean"))
+                .forEach(location -> location.setPerturbator(new InvPerturbatorImpl()));
+        this.locations.stream().filter(location -> location.getType().equals("Numerical"))
+                .forEach(location -> location.setPerturbator(new AddNPerturbatorImpl(1)));
+
+        this.antifragileLocation = new ArrayList<>();
+        this.robustLocation = new ArrayList<>();
+        this.weakLocation = new ArrayList<>();
+
+        this.readFile("results/" + this.manager.getName() + "/IntegerAddOne_RandomExplorer_analysis_graph_data.txt");
+        this.readFile("results/" + this.manager.getName() + "/BooleanNegation_RandomExplorer_analysis_graph_data.txt");
+
+        System.out.println(this.antifragileLocation);
+        System.out.println(this.robustLocation);
+        System.out.println(this.weakLocation);
     }
 
+    private void readFile(String path) {
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            br.readLine();
+            br.readLine();
+            br.readLine();//Trash unused lines
+            int nbRandomRate = br.readLine().split(" ").length - 3;
+            br.readLine();
+            br.readLine();
+            br.readLine();//Trash unused lines
+            int nbLocation = Integer.parseInt(br.readLine().split(" ")[0]);
+            br.readLine();//Trash unused lines
+
+            for (int loc = 0; loc < nbLocation; loc++) {
+                boolean isAntifragile = true;
+                boolean added = false;
+                int indexLoc = -1;
+                for (int random = 0; random < nbRandomRate ; random++) {
+                    String[] line = br.readLine().split(" ");
+                    float success = Float.parseFloat(line[line.length - 1].replace(",", "."));
+                    indexLoc = Integer.parseInt(line[2]);
+                    if (success < 50.0) {
+                        this.weakLocation.add(indexLoc);
+                        added = true;
+                        break;
+                    } else if (success < 100.0) {
+                        isAntifragile = false;
+                    }
+                }
+                if (!added) {
+                    if (isAntifragile)
+                        this.antifragileLocation.add(indexLoc);
+                    else
+                        this.robustLocation.add(indexLoc);
+                }
+            }
+
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setUpLocations() {
+        this.locations.forEach(location -> location.setEnactor(new NeverEnactorImpl()));//Clean up
+
+        List<Integer> indices = new ArrayList<>();
+        if (this.classOfLocation.contains("Antifragile"))
+            indices.addAll(this.antifragileLocation);
+        if (this.classOfLocation.contains("Robust"))
+            indices.addAll(this.robustLocation);
+        if (this.classOfLocation.contains("Weak"))
+            indices.addAll(this.robustLocation);
+
+        this.locations.stream().filter(location -> location.getType().equals(this.currentTypeOfLocation) && indices.contains(location.getLocationIndex()))
+                .forEach(location -> location.setEnactor(new RandomEnactorImpl(rnd)));
+    }
+
+    public void incRnd() {
+        this.rnd = this.rnd + this.step < 1.0f ? this.rnd + this.step : 1.0f;
+        this.setUpLocations();
+    }
+
+    public void decRnd() {
+        this.rnd = this.rnd - this.step > 0.0f ? this.rnd - this.step : 0.0f;
+        this.setUpLocations();
+    }
+
+    public float getRnd() {
+        return this.rnd;
+    }
 
     public float runAllTask() {
-        int nbSuccess = IntStream.range(0, numberOfTask).reduce(0, (acc, indexTask) -> acc + runLocations(indexTask));
-        System.out.println(nbSuccess);
-        return (float) nbSuccess / (float)(locations.size() * numberOfTask) * 100;
+        try {
+            PerturbationEngine.loggers.get("gui").reset();
+            this.locations.forEach(location -> PerturbationEngine.loggers.get("gui").logOn(location));
+            this.manager = (Manager) this.classManager.getConstructor(int.class, int.class, int.class).newInstance(this.numberOfTask, this.size, (int) System.currentTimeMillis());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        int nbSuccess = IntStream.range(0, numberOfTask).reduce(0, (acc, indexTask) -> acc + runPerturbation(indexTask));
+//        int nbSuccess = IntStream.range(0, numberOfTask).reduce(0, (acc, indexTask) -> acc + runLocations(indexTask));
+
+        this.avgPerturbationPerExec = (double) IntStream.range(0, locations.size())
+                .reduce(0, (acc, indexLocation) ->
+                        acc + PerturbationEngine.loggers.get("gui").getEnactions(locations.get(indexLocation))
+                ) / (double) numberOfTask;
+
+        return (float) nbSuccess / (float) (numberOfTask) * 100;
+//        return (float) nbSuccess / (float) (locations.size()*numberOfTask) * 100;
     }
 
     private int runLocations(int indexTask) {
-        return IntStream.range(0, locations.size())
-                        .reduce(0, (acc, indexLocation) ->
-                                acc + runLocation(indexTask, locations.get(indexLocation))
-                        );
+        return IntStream.range(0, locations.size()).reduce(0, (acc, indexLocation) ->
+                acc + runLocation(indexTask, this.locations.get(indexLocation))
+        );
     }
 
     private int runLocation(int indexTask, PerturbationLocation location) {
-        boolean success;
         location.setEnactor(new RandomEnactorImpl(rnd));
-        success = runPerturbation(indexTask);
+        int assertion = runPerturbation(indexTask);
         location.setEnactor(new NeverEnactorImpl());
-        return success ? 1 : 0;
+        return assertion;
     }
 
-    private boolean runPerturbation(int indexTask) {
+    private int runPerturbation(int indexTask) {
         ExecutorService executor = Executors.newSingleThreadExecutor();
+        accExec++;
         try {
-            Callable instanceRunner = manager.getCallable(manager.getTask(indexTask));
+            Callable instanceRunner = this.manager.getCallable(manager.getTask(indexTask));
             Future future = executor.submit(instanceRunner);
             try {
-                int[] output = (int[])(future.get(15, TimeUnit.SECONDS));
-                boolean assertion = oracle.assertPerturbation(manager.getTask(indexTask), output);
-                executor.shutdownNow();
-                return assertion;
+                Object output = future.get(15, TimeUnit.SECONDS);
+                if (this.manager.getOracle().assertPerturbation(manager.getTask(indexTask), output)) {
+                    executor.shutdownNow();
+                    accExecSuccess++;
+                    return 1;
+                } else {
+                    executor.shutdownNow();
+                    return 0;
+                }
             } catch (TimeoutException e) {
                 future.cancel(true);
                 System.err.println("Time out!");
                 executor.shutdownNow();
-                return false;
+                return 0;
             }
         } catch (Exception | Error e) {
             executor.shutdownNow();
-            return false;
+            return 0;
         }
     }
 
--- src/main/java/gui/Window.java
+++ src/main/java/gui/Window.java
@@ -1,116 +1,225 @@
 package gui;
 
+import quicksort.QuickSortManager;
+
 import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.Mixer;
-import javax.sound.sampled.Port;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
-
-import static javax.sound.sampled.FloatControl.Type.MASTER_GAIN;
+import java.lang.reflect.Field;
 
 /**
  * Created by bdanglot on 13/05/16.
  */
 public class Window extends JFrame {
 
-    private Control c;
-
     private JPanel panel;
 
-    private JLabel label;
-
-    private final int cx = 200;
-    private final int cy = 300;
+    private final int cx;
+    private final int cy;
     private final int r = 100;
+
+    private JLabel labelAccExec;
+
+    private JLabel labelAccExecSuccess;
+
+    private JLabel labelavgPertubationPerExecution;
+
+    private JLabel labelNbTask;
+
+    private JSlider sliderNb;
+
+    private JLabel labelSize;
+
+    private JSlider sliderSize;
 
     private Model model;
 
-    private JTextArea text;
+    private JTextArea rand;
+
+    private JPanel panelImg;
+
+    private BufferedImage img;
+
+    private ButtonGroup typeOfPerturbation;
+
+
 
     public Window(Model m) {
         super();
         this.model = m;
-        this.label = new JLabel();
-        this.c = new Control(m, label);
+        this.rand = new JTextArea(String.format("%.2f", this.model.getRnd()));
+        this.rand.setEditable(false);
+
+        this.labelAccExec = new JLabel(String.valueOf(this.model.getAccExec()));
+        this.labelAccExecSuccess = new JLabel(String.valueOf(this.model.getAccExecSuccess()));
+        this.labelavgPertubationPerExecution = new JLabel(String.valueOf(this.model.getAvgPerturbationPerExec()));
+
+        try {
+            this.img = ImageIO.read(new File("resources/img/success_meter.png"));
+        } catch (IOException e) {e.printStackTrace();}
 
         this.panel = new JPanel();
-        this.panel.add(c);
+        this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.PAGE_AXIS));
 
-        this.text = new JTextArea("toto");
+        this.panel.add(this.buildPanelLabel());
+        this.panel.add(this.buildTaskManager());
+        this.panel.add(this.buildLocationManager());
 
-        this.panel.add(text);
-        this.panel.add(label);
-        this.panel.setPreferredSize(new Dimension(400,350));
+        this.panel.add(new JLabel());
+        this.panelImg = new JPanel();
+        this.panelImg.setPreferredSize(new Dimension(this.img.getWidth(), this.img.getHeight()));
+        this.panel.add(this.panelImg);
+        this.panel.add(new JLabel());
+
         this.panel.setBackground(Color.WHITE);
-
         this.setResizable(false);
         this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setContentPane(panel);
-        this.setPreferredSize(new Dimension(400,350));
         this.pack();
 
-        this.text.addKeyListener(new KeyAdapter() {
+        this.cy = this.panel.getHeight() - r / 2;
+        this.cx = this.panel.getWidth() / 2 ;
 
+        Graphics2D g2D = (Graphics2D) this.panel.getGraphics();
+        g2D.clearRect(this.panelImg.getX(), this.panelImg.getY(),
+                this.panelImg.getX()+this.panelImg.getWidth(), this.panelImg.getY() + this.panelImg.getHeight());
+        int xImg = (this.panel.getWidth() / 2) - (this.img.getWidth() / 2);
+        g2D.drawImage(img, xImg , this.panel.getHeight() - this.img.getHeight() , null);
 
+        this.setIgnoreRepaint(true);
+
+        this.addListeners();
+    }
+
+    private void addListeners() {
+        this.addMouseWheelListener(new MouseAdapter() {
             @Override
-            public void keyPressed(KeyEvent e) {
-                System.out.println("pressed " + e.getKeyCode());
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                System.out.println("released " + e.getKeyCode());
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                switch (e.getWheelRotation()) {
+                    case -1:
+                        model.incRnd();
+                        break;
+                    case 1:
+                        model.decRnd();
+                        break;
+                }
+                rand.setText(String.format("%.3f", model.getRnd()));
             }
         });
 
-        Graphics2D g = (Graphics2D) this.panel.getGraphics();
-        g.clearRect(0, this.c.getHeight() + 10, 400, 350);
-        this.drawSuccessMeter(g);
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                try {
+                    Class<?> clazz = e.getClass();
+                    Field fieldRawCode = clazz.getDeclaredField("rawCode");
+                    fieldRawCode.setAccessible(true);
+                    long rawCode = (long) fieldRawCode.get(e);
+                    if (rawCode == 123)
+                        model.incRnd();
+                    else if (rawCode == 122)
+                        model.decRnd();
+                    rand.setText(String.format("%.3f", model.getRnd()));
+                } catch (NoSuchFieldException | IllegalAccessException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
     }
 
-    private void drawSuccessMeter(Graphics2D g) {
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File("resources/img/success_meter.png"));
-            g.drawImage(img, -45, 350 - img.getHeight(), null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private JPanel buildPanelLabel() {
+        JPanel panelLabel = new JPanel();
+        panelLabel.setLayout(new GridLayout(3,3));
+
+        panelLabel.add(new JLabel());
+        panelLabel.add(this.rand);
+        panelLabel.add(new JLabel());
+
+        panelLabel.add(this.labelavgPertubationPerExecution);
+        panelLabel.add(new JLabel());
+        panelLabel.add(this.labelAccExec);
+
+        panelLabel.add(new JLabel());
+        panelLabel.add(new JLabel());
+        panelLabel.add(this.labelAccExecSuccess);
+        return panelLabel;
+    }
+
+    private JPanel buildTaskManager() {
+        JPanel taskManager = new JPanel();
+        this.labelNbTask = new JLabel(String.valueOf(this.model.getNumberOfTask()));
+        this.sliderNb = new JSlider(JSlider.HORIZONTAL, 1, 200, this.model.getNumberOfTask());
+        this.labelSize = new JLabel(String.valueOf(this.model.getSize()));
+        this.sliderSize = new JSlider(JSlider.HORIZONTAL, 1, 1000, this.model.getSize());
+        this.sliderNb.addChangeListener(event -> this.model.setNumberOfTask(sliderNb.getValue()));
+        this.sliderSize.addChangeListener(event -> this.model.setSize(sliderSize.getValue()));
+        taskManager.add(this.labelNbTask);
+        taskManager.add(this.sliderNb);
+        taskManager.add(this.labelSize);
+        taskManager.add(this.sliderSize);
+        return taskManager;
+    }
+
+    private JPanel buildLocationManager() {
+        JPanel panelLocation = new JPanel();
+        this.typeOfPerturbation = new ButtonGroup();
+        JRadioButton integer = new JRadioButton("Numerical", true);
+        integer.addActionListener(e -> this.model.setType("Numerical"));
+        this.typeOfPerturbation.add(integer);
+        JRadioButton booleanb = new JRadioButton("Boolean");
+        booleanb.addActionListener(e -> this.model.setType("Boolean"));
+        this.typeOfPerturbation.add(booleanb);
+        panelLocation.add(integer);
+        panelLocation.add(booleanb);
+
+        panelLocation.add(new Checkbox("Antifragile", null, true));
+        panelLocation.add(new Checkbox("Robust"));
+        panelLocation.add(new Checkbox("Weak"));
+
+        return panelLocation;
     }
 
     public void run() {
         Graphics2D g2D = (Graphics2D) this.panel.getGraphics();
+
+        this.labelAccExec.setText(String.valueOf(this.model.getAccExec()));
+        this.labelAccExecSuccess.setText(String.valueOf(this.model.getAccExecSuccess()));
+        this.labelavgPertubationPerExecution.setText(String.format("%.2f", this.model.getAvgPerturbationPerExec()));
+        this.labelSize.setText(String.valueOf(this.model.getSize()));
+        this.labelNbTask.setText(String.valueOf(this.model.getNumberOfTask()));
+
         double percentage = this.model.runAllTask();
-        System.out.println(String.format("%.2f", percentage) + " % of success");
-        this.panel.setBackground(Color.WHITE);
-        g2D.clearRect(0, this.c.getHeight() + 15, 400, 350);
-        this.drawSuccessMeter(g2D);
+
+        g2D.clearRect(this.panelImg.getX(), this.panelImg.getY(),
+                this.panelImg.getX()+this.panelImg.getWidth(), this.panelImg.getY() + this.panelImg.getHeight());
+        int xImg = (this.panel.getWidth() / 2) - (this.img.getWidth() / 2);
+        g2D.drawImage(img, xImg , this.panel.getHeight() - this.img.getHeight() , null);
         g2D.setStroke(new BasicStroke(3));
         g2D.setColor(Color.red);
-        double a = ( (percentage + 900 ) * 180 / 100) * Math.PI / 180F;
-        float x = (float)(cx + r * Math.cos(a));
-        float y = (float)(cy + r * Math.sin(a));
+        double a = ((percentage + 900) * 180 / 100) * Math.PI / 180F;
+        float x = (float) (cx + r * Math.cos(a));
+        float y = (float) (cy + r * Math.sin(a));
         g2D.draw(new Line2D.Float(cx, cy, x, y));
+
     }
 
-    public static void main(String[] args) {
-        Model m = new Model();
+    public static void launch(Class<?> clazz) {
+        Model m = new Model(clazz);
         Window w = new Window(m);
         while (true) {
+//            w.requestFocus();
             w.run();
         }
+    }
+
+    public static void main() {
+        launch(QuickSortManager.class);
     }
 
 }
