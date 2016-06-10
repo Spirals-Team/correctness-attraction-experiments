ba81b4ea0d1213aff435ce6a1007fdf12bce7961
bdanglot <bdanglot@gmail.com>
2016-06-10 15:54:11 +0200

    feat: generation of new task if required

--- src/main/java/experiment/ManagerImpl.java
+++ src/main/java/experiment/ManagerImpl.java
@@ -106,6 +106,12 @@ public abstract class ManagerImpl<T, P> implements Manager<T, P> {
         this.locations = locations;
     }
 
+    @Override
+    public T getTask(int indexOfTask) {
+        this.tasks.add(this.generateOneTask());
+        this.indexTasks.add(this.tasks.size()-1);
+        return this.getTask(this.tasks.size()-1);
+    }
 
     @Override
     public List<PerturbationLocation> getLocations(String filter) {
