9613fa58a9128c52d3e1350ee58fa410b754b539
bdanglot <bdanglot@gmail.com>
2016-06-14 10:28:14 +0200

    remove spoon and spooning

--- src/script/light-install.sh
+++ src/script/light-install.sh
@@ -1,32 +1,8 @@
 #!/usr/bin/env bash
 
-jPerturb=jPerturb/target/jPerturb-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-perturbation=jPerturb/src/main/java/perturbation/
-processors=processor.AssignmentProcessor:processor.VariableCaster:processor.PerturbationProcessor
-m2_repo=$HOME/.m2/repository
-
-#Install Sponn
-git clone https://github.com/INRIA/spoon.git
-cd spoon
-mvn install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
-
-cd ..
-
 #Install jPerturb
 git clone http://github.com/Spirals-Team/jPerturb.git
-cd jPerturb
-mvn package -Dmaven.test.skip=true
-
-cd ..
-
-#Spoon little exp we spoon only Quicksort in order to add perturbation package to jPerturb-experiments.
-i=(quicksort/QuickSort.java)
-#i=(quicksort/QuickSort.java md5/MD5.java mersenne/MersenneTwister.java sudoku/Sudoku.java zip/LZW.java canny/CannyEdgeDetector.java lcs/LCS.java)
-for file in "${i[@]}"
-do
-    echo "java -jar $jPerturb -i src/main/java/$file:$perturbation -o src/main/java --with-imports -p $processors"
-    java -jar $jPerturb -i src/main/java/$file:$perturbation -o src/main/java --with-imports -p processor.RenameProcessor:$processors
-done
+cp -r jPerturb/src/main/java/perturbation src/main/java/
 
 #Now RM huge packages
 path=src/main/java
