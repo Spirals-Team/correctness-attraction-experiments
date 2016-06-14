5b60db85609cd2e43dd496272f16ebdbd44189db
bdanglot <bdanglot@gmail.com>
2016-06-14 10:09:30 +0200

    feat: light-install script to run only little xp

---
+++ src/script/light-install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+jPerturb=jPerturb/target/jPerturb-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+perturbation=jPerturb/src/main/java/perturbation/
+processors=processor.AssignmentProcessor:processor.VariableCaster:processor.PerturbationProcessor
+m2_repo=$HOME/.m2/repository
+
+#Install Sponn
+git clone https://github.com/INRIA/spoon.git
+cd spoon
+mvn install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
+
+cd ..
+
+#Install jPerturb
+git clone http://github.com/Spirals-Team/jPerturb.git
+cd jPerturb
+mvn package -Dmaven.test.skip=true
+
+cd ..
+
+#Spoon little exp we spoon only Quicksort in order to add perturbation package to jPerturb-experiments.
+i=(quicksort/QuickSort.java)
+#i=(quicksort/QuickSort.java md5/MD5.java mersenne/MersenneTwister.java sudoku/Sudoku.java zip/LZW.java canny/CannyEdgeDetector.java lcs/LCS.java)
+for file in "${i[@]}"
+do
+    echo "java -jar $jPerturb -i src/main/java/$file:$perturbation -o src/main/java --with-imports -p $processors"
+    java -jar $jPerturb -i src/main/java/$file:$perturbation -o src/main/java --with-imports -p processor.RenameProcessor:$processors
+done
+
+#Now RM huge packages
+path=src/main/java
+packages=(bitcoin classifier laguerre rc4 rsa sat simplex torrent)
+for file in "${packages[@]}"
+do
+    echo "rm -rf $path/$file"
+    rm -rf $path/$file
+done
+
+#Rewriting the pom.xml in order to remove dependencies
+head -n 28 pom.xml > npom.xml
+echo "\t</dependencies>\n</project>" >> npom.xml
+rm -f pom.xml
+mv npom.xml pom.xml
+
+mvn compile
