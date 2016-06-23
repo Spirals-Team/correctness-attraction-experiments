6c850a1e2025a57088c55ac533a2f3a2c05cd0e5
bdanglot <bdanglot@gmail.com>
2016-06-23 17:16:52 +0200

    add javadoc

--- src/main/java/experiment/CallableImpl.java
+++ src/main/java/experiment/CallableImpl.java
@@ -7,12 +7,19 @@ import java.util.concurrent.Callable;
  */
 public abstract class CallableImpl<T,P> implements Callable<P> {
 
+    /**
+     * Input of the execution under perturbation, usually provided by the manager
+     */
     protected T input;
 
     public CallableImpl(T input) {
         this.input = input;
     }
 
+    /**
+     * @return the output expected by the oracle
+     * @throws Exception
+     */
     @Override
     public abstract P call() throws Exception;
 }
