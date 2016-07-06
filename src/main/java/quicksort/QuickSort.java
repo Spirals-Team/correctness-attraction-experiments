721a0eb37ae65c95417730b96b4e0f3a65891baf
bdanglot <bdanglot@gmail.com>
2016-07-06 16:55:27 +0200

    update WCET bensh

--- src/main/java/quicksort/QuickSort.java
+++ src/main/java/quicksort/QuickSort.java
@@ -1,13 +1,64 @@
 package quicksort;
 
+import perturbation.PerturbationEngine;
+import perturbation.location.PerturbationLocation;
+import perturbation.location.PerturbationLocationImpl;
+
 /**
  * Created by bdanglot on 26/05/16.
  */
 public class QuickSort {
 
-    public static void sort(int [] array, int beg, int end) {
+    static {
+        QuickSort.initPerturbationLocation0();
+    }
+
+    public static PerturbationLocation __L0;
+
+    public static PerturbationLocation __L1;
+
+    static int rcount = 0;
+
+    static int count = 0;
+
+    public static void r_sort(int[] array, int beg, int end) {
+
+        rcount++;
+
         int left = beg, right = end;
-        int pivot = array[beg + ((end - beg) / 2)];
+//        int pivot = array[beg + ((end - beg) / 2)];
+        int pivot = PerturbationEngine.pint(__L1, array[PerturbationEngine.pint(__L0, beg)]);
+
+        while (left <= right) {
+
+            while (array[left] < pivot) {
+                left++;
+            }
+
+            while (array[right] > pivot) {
+                right--;
+            }
+
+            if (left <= right) {
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+
+        }
+        if (beg < right)
+            r_sort(array, beg, right);
+        if (end > left)
+            r_sort(array, left, end);
+    }
+
+    public static void sort(int[] array, int beg, int end) {
+
+        count++;
+
+        int left = beg, right = end;
+//        int pivot = array[beg + ((end - beg) / 2)];
+        int pivot = array[beg];
 
         while (left <= right) {
 
@@ -32,11 +83,15 @@ public class QuickSort {
             sort(array, left, end);
     }
 
-    private static void swap(int [] array, int i, int j) {
+    private static void swap(int[] array, int i, int j) {
         int x = array[i];
         array[i] = array[j];
         array[j] = x;
     }
 
+    static private void initPerturbationLocation0() {
+        __L0 = new PerturbationLocationImpl("QuickSort.java:9", 0, "Numerical");
+        __L1 = new PerturbationLocationImpl("QuickSort.java:9", 0, "Numerical");
+    }
 
 }
--- src/main/java/quicksort/WCETBensh.java
+++ src/main/java/quicksort/WCETBensh.java
@@ -2,30 +2,31 @@ package quicksort;
 
 import perturbation.enactor.NeverEnactorImpl;
 import perturbation.enactor.RandomEnactorImpl;
+import perturbation.enactor.RandomUniqueEpsilonEnactor;
 import perturbation.location.PerturbationLocation;
 import perturbation.perturbator.AddNPerturbatorImpl;
 import perturbation.perturbator.NothingPerturbatorImpl;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
 /**
  * Created by bdanglot on 23/06/16.
  */
 public class WCETBensh {
 
+    private static final int NB_ARRAY = 100;
+    private static final int SIZE_ARRAY = 4000;
+
     public static void main(String[] args) {
-        QuickSortManager manager = new QuickSortManager(100, 100000);
+        QuickSortManager manager = new QuickSortManager(NB_ARRAY, SIZE_ARRAY);
         run(manager);
     }
 
     private static List<PerturbationLocation> getLocationPivot() {
         List<PerturbationLocation> locations = new ArrayList<>();
-        locations.add(QuickSortInstr.__L2);
-        locations.add(QuickSortInstr.__L3);
-        locations.add(QuickSortInstr.__L4);
-        locations.add(QuickSortInstr.__L5);
-        locations.add(QuickSortInstr.__L6);
-        locations.add(QuickSortInstr.__L7);
         locations.add(QuickSortInstr.__L8);
         return locations;
     }
@@ -33,33 +34,36 @@ public class WCETBensh {
     private static void run(QuickSortManager manager) {
         long accTime = 0L;
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < NB_ARRAY; i++) {
             int[] task = manager.getTask(i);
-            long time = System.currentTimeMillis();
             QuickSort.sort(task, 0, task.length - 1);
-            long timeElapsed = System.currentTimeMillis() - time;
-            accTime += timeElapsed;
+            QuickSort.count = 0;
+            QuickSort.sort(task, 0, task.length - 1);
+            accTime += QuickSort.count;
+            QuickSort.count = 0;
         }
-        System.out.println(accTime / 100);
+        System.out.println((double)accTime / (double)NB_ARRAY);
 
-        for (PerturbationLocation perturbationLocation : getLocationPivot()) {
-            perturbationLocation.setEnactor(new RandomEnactorImpl());
-            perturbationLocation.setPerturbator(new AddNPerturbatorImpl(1));
+        accTime = 0L;
+        for (int i = 0; i < NB_ARRAY; i++) {
+            int[] task = manager.getTask(i);
+            QuickSort.sort(task, 0, task.length - 1);
+            QuickSort.count = 0;
 
-            accTime = 0L;
-            for (int i = 0; i < 100; i++) {
-                int[] task = manager.getTask(i);
-                long time = System.currentTimeMillis();
-                QuickSortInstr.sort(task, 0, task.length - 1);
-                long timeElapsed = System.currentTimeMillis() - time;
-                accTime += timeElapsed;
+            QuickSort.__L0.setEnactor(new RandomEnactorImpl());
+            QuickSort.__L0.setPerturbator(new AddNPerturbatorImpl(1));
 
-            }
-            System.out.println(perturbationLocation.getLocationIndex() + " " + accTime / 100);
+            QuickSort.r_sort(task, 0, task.length - 1);
+            accTime +=  QuickSort.rcount ;
+            QuickSort.rcount  = 0;
 
-            perturbationLocation.setEnactor(new NeverEnactorImpl());
-            perturbationLocation.setPerturbator(new NothingPerturbatorImpl());
+            QuickSort.__L0.setEnactor(new NeverEnactorImpl());
+            QuickSort.__L0.setPerturbator(new NothingPerturbatorImpl());
         }
+        System.out.println((double)accTime / (double)NB_ARRAY);
+
+        QuickSort.__L0.setEnactor(new NeverEnactorImpl());
+        QuickSort.__L0.setPerturbator(new NothingPerturbatorImpl());
 
 
     }
