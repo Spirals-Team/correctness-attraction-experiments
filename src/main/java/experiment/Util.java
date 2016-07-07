407da1c994236fcb21f2a5e910dc5457055ffeff
bdanglot <bdanglot@gmail.com>
2016-07-07 13:19:28 +0200

    classes is now a new list (stateless)

--- src/main/java/experiment/Util.java
+++ src/main/java/experiment/Util.java
@@ -15,11 +15,9 @@ public class Util {
 
     private static ClassLoader loader = ClassLoader.getSystemClassLoader();
 
-    private static List<Class> classes = new ArrayList<>();
-
     public static List<PerturbationLocation> getAllLocations(String project, String packagaPath, String type) {
         final List<PerturbationLocation> locations = new ArrayList<>();
-        iterateFolders(project, packagaPath);
+        List<Class> classes = iterateFolders(new ArrayList<>(), project, packagaPath);
 //        System.out.println("Number of classes " +  classes.size());
         classes.stream().forEach(clazz -> {
                     PerturbationLocationImpl.getLocationFromClass(clazz).forEach(location -> {
@@ -31,12 +29,12 @@ public class Util {
         return locations.stream().filter(location -> location.getType().equals(type)).collect(Collectors.toList());
     }
 
-    private static void iterateFolders(String path, String currentPackage) {
+    private static List<Class> iterateFolders(List<Class> classes, String path, String currentPackage) {
         File root = new File(path);
         assert root.listFiles() != null;
         for (File subFile : root.listFiles()) {
             if (subFile.isDirectory())
-                iterateFolders(path + subFile.getName() + "/", currentPackage + "." + subFile.getName());
+                iterateFolders(classes, path + subFile.getName() + "/", currentPackage + "." + subFile.getName());
             else if (isJava(subFile.getName())) {
                 try {
                     Class<?> clazz = loader.loadClass(currentPackage + "." + removeExt(subFile.getName()));
@@ -47,6 +45,7 @@ public class Util {
                 }
             }
         }
+        return classes;
     }
 
     private static String removeExt(String name) {
@@ -56,7 +55,6 @@ public class Util {
     private static boolean isJava(String name) {
         return name.endsWith(".java");
     }
-
 
     public static String getStringPerc(long nb, long total) {
         double perc = perc(nb, total);
