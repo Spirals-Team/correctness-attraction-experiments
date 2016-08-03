fccf73f94752612607b4b917ae18afb5bcc824c0
bdanglot <bdanglot@gmail.com>
2016-08-03 13:22:17 +0200

    test bandit

--- src/test/java/explorer/TestBandit.java
+++ src/test/java/explorer/TestBandit.java
@@ -6,9 +6,9 @@ import experiment.exploration.Exploration;
 import experiment.exploration.IntegerExplorationPlusOne;
 import experiment.explorer.bandit.BanditExplorer;
 import experiment.explorer.bandit.budget.Budget;
-import experiment.explorer.bandit.policy.Policy;
 import experiment.explorer.bandit.budget.TimeBudget;
-import experiment.explorer.bandit.policy.UCBPolicy;
+import experiment.explorer.bandit.policy.Policy;
+import experiment.explorer.bandit.policy.RandomPolicy;
 import org.junit.Test;
 import quicksort.QuickSortManager;
 
@@ -26,17 +26,18 @@ public class TestBandit {
 		Manager manager = new QuickSortManager(1, 25);
 		Main.manager = manager;
 		Exploration exploration = new IntegerExplorationPlusOne();
-		Policy policy = new UCBPolicy(manager.getLocations().size());
+//		Policy policy = new UCBPolicy(manager.getLocations().size());
+		Policy policy = new RandomPolicy(manager.getLocations().size());
 		Budget budget = new TimeBudget(1000);
 		BanditExplorer explorer1 = new BanditExplorer(exploration, manager, policy, budget);
 
-		assertEquals(explorer1.outStateBandit(), BanditExplorer.buildBanditFromString(0, explorer1.outStateBandit().split(" ")).outStateBandit());
+		assertEquals(explorer1.outStateBandit(), BanditExplorer.buildBanditFromString(0, explorer1.outStateBandit().split(" "), policy).outStateBandit());
 
 		String initState = explorer1.outStateBandit();
 
 		explorer1.run();
 
-		assertEquals(explorer1.outStateBandit(), BanditExplorer.buildBanditFromString(0, explorer1.outStateBandit().split(" ")).outStateBandit());
+		assertEquals(explorer1.outStateBandit(), BanditExplorer.buildBanditFromString(0, explorer1.outStateBandit().split(" "), policy).outStateBandit());
 		assertNotEquals(initState, explorer1.outStateBandit());
 	}
 
